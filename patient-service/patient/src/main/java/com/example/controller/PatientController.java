a0fd04d0b451054938535f0e503cee3d8eadbebf
daan153 <creative.in47@gmail.com>
2025-11-16 15:46:55 +0530

    controller and repository created for patients

---
+++ patient-service/patient/src/main/java/com/example/controller/PatientController.java
@@ -0,0 +1,48 @@
+package com.example.controller;
+
+
+import com.example.model.Patient;
+import com.example.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.hibernate.Hibernate.map;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> getAllPatients() {
+        return ResponseEntity.ok(patientService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable Long id) {
+        return patientService.findById(id)
+        .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+        Patient savedPatient = patientService.save(patient);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPatient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
+        patientService.deleteById();
+        return ResponseEntity.noContent().build();
+    }
+}
