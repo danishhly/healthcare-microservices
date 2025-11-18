02faec33b20733a313ae0a421168506c4b2f4c3d
daan153 <creative.in47@gmail.com>
2025-11-18 09:32:04 +0530

    update patient service code

--- patient-service/src/main/java/com/example/controller/PatientController.java
+++ patient-service/src/main/java/com/example/controller/PatientController.java
@@ -1,8 +1,8 @@
 package com.example.controller;
 
-
 import com.example.model.Patient;
 import com.example.service.PatientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.hibernate.Hibernate.map;
-
 @RestController
 @RequestMapping("/patients")
 @RequiredArgsConstructor
 public class PatientController {
     private final PatientService patientService;
-
 
     @GetMapping
     public ResponseEntity<List<Patient>> getAllPatients() {
@@ -27,14 +24,28 @@ public class PatientController {
     @GetMapping("/{id}")
     public ResponseEntity<Patient> getPatientById(@PathVariable Long id) {
         return patientService.findById(id)
-        .map(ResponseEntity::ok)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+    public ResponseEntity<Patient> createPatient(@Valid @RequestBody Patient patient) {
         Patient savedPatient = patientService.save(patient);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedPatient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable Long id, @Valid @RequestBody Patient patientDetails) {
+        return patientService.updatePatient(id, patientDetails)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Patient> partialUpdatePatient(@PathVariable Long id, @RequestBody Patient patientDetails) {
+        return patientService.partialUpdatePatient(id, patientDetails)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
