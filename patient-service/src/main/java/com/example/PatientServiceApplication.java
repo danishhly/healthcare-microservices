c06fb29edd6053eb065ed79cab65bb76d32e2754
daan153 <creative.in47@gmail.com>
2025-11-21 14:55:07 +0530

    fix 2

--- patient-service/src/main/java/com/example/PatientServiceApplication.java
+++ patient-service/src/main/java/com/example/PatientServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PatientServiceApplication {
+public class
+PatientServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PatientServiceApplication.class, args);
