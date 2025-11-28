e33aa138203eea2ea71cf7dba99248aa3eaab133
Daan153 <creative.in47@gmail.com>
2025-11-28 13:45:07 +0530

    path variableupdated in patient controller

--- patient-service/src/main/java/com/example/controller/PatientController.java
+++ patient-service/src/main/java/com/example/controller/PatientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/patients")
+@RequestMapping("/api/v1/patients")
 @RequiredArgsConstructor
 @Validated
 public class PatientController {
